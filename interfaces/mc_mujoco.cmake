42af8f720a08e60d51ec8a93b3714e81c47f2ddf
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-02-07 14:04:27 +0900

    Add HRP5/HRP4CR MuJoCo models if needed

--- interfaces/mc_mujoco.cmake
+++ interfaces/mc_mujoco.cmake
@@ -6,3 +6,19 @@ AddProject(mc_mujoco
   CMAKE_ARGS -DMUJOCO_ROOT_DIR=${MUJOCO_ROOT_DIR}
   DEPENDS mc_rtc
 )
+
+if(WITH_HRP5)
+  AddProject(hrp5p_mj_description
+    GITHUB_PRIVATE isri-aist/hrp5p_mj_description
+    GIT_TAG origin/main
+    DEPENDS mc_mujoco
+  )
+endif()
+
+if(WITH_HRP4CR)
+  AddProject(hrp4cr_mj_description
+    GITHUB_PRIVATE isri-aist/hrp4cr_mj_description
+    GIT_TAG origin/main
+    DEPENDS mc_mujoco
+  )
+endif()
