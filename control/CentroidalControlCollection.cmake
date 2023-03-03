7455c67baf1ee415ee01a94988d8aeac79a662e6
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-03-03 15:40:20 +0900

    [CentroidalControlCollection] Update dependencies

--- control/CentroidalControlCollection.cmake
+++ control/CentroidalControlCollection.cmake
@@ -1,8 +1,8 @@
-include(${CMAKE_CURRENT_LIST_DIR}/../solvers/QpSolverCollection.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ForceControlCollection.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/NMPC.cmake)
 
 AddProject(CentroidalControlCollection
   GITHUB isri-aist/CentroidalControlCollection
   GIT_TAG origin/master
-  DEPENDS QpSolverCollection NMPC
+  DEPENDS ForceControlCollection NMPC
 )
