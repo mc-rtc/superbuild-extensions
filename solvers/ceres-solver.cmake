a952937470f3c9d1e9652c6bd002e33120ec59b8
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-05-09 12:30:36 +0200

    [ceres-solver] Skip examples and benchmarks build

--- solvers/ceres-solver.cmake
+++ solvers/ceres-solver.cmake
@@ -3,5 +3,6 @@ AptInstall(libgoogle-glog-dev)
 AddProject(ceres-solver
   GITHUB ceres-solver/ceres-solver
   GIT_TAG 2.1.0
+  CMAKE_ARGS -DBUILD_BENCHMARKS:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF
   SKIP_TEST
 )
