950160dfa38783a07532a6716fef48cdac47b59a
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-05-23 15:59:24 +0900

    Avoid usage of DEFER_CALL
    This makes it more difficult to use those packages in other extensions.
    The drawback is that we need to ensure CnoidRosUtils.cmake is included
    before LocomanipController if needed

--- controllers/LocomanipController.cmake
+++ controllers/LocomanipController.cmake
@@ -1,15 +1,11 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../controllers/BaseLineWalkingController.cmake)
 
-function(AddLocomanipController)
-  set(DEPS BaseLineWalkingController)
-  if(TARGET CnoidRosUtils)
-    list(APPEND DEPS CnoidRosUtils)
-  endif()
-  AddProject(LocomanipController
-    GITHUB isri-aist/LocomanipController
-    GIT_TAG origin/master
-    DEPENDS ${DEPS}
-  )
-endfunction()
-
-cmake_language(DEFER CALL AddLocomanipController)
+set(LMC_DEPS BaseLineWalkingController)
+if(TARGET CnoidRosUtils)
+  list(APPEND LMC_DEPS CnoidRosUtils)
+endif()
+AddProject(LocomanipController
+  GITHUB isri-aist/LocomanipController
+  GIT_TAG origin/master
+  DEPENDS ${LMC_DEPS}
+)
--- simulation/CnoidRosUtils.cmake
+++ simulation/CnoidRosUtils.cmake
@@ -1,12 +1,10 @@
-function(AddCnoidRosUtils)
-  if(TARGET choreonoid AND WITH_ROS_SUPPORT)
-    AptInstall(ros-${ROS_DISTRO}-eigen-conversions)
-    AddProject(CnoidRosUtils
-      GITHUB isri-aist/CnoidRosUtils
-      GIT_TAG origin/master
-      DEPENDS choreonoid
-    )
-  endif()
-endfunction()
+if(NOT TARGET choreonoid OR NOT WITH_ROS_SUPPORT)
+  message(FATAL_ERROR "CnoidRosUtils requires choreonoid and ROS support")
+endif()
 
-cmake_language(DEFER CALL AddCnoidRosUtils)
+AptInstall(ros-${ROS_DISTRO}-eigen-conversions)
+AddProject(CnoidRosUtils
+  GITHUB isri-aist/CnoidRosUtils
+  GIT_TAG origin/master
+  DEPENDS choreonoid
+)
