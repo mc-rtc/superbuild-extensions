a490dd46d5ac7bb262d75c03d255543b6847f73b
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-08-02 09:59:44 +0900

    Go back to main repo of osqp-eigen

--- solvers/osqp-eigen.cmake
+++ solvers/osqp-eigen.cmake
@@ -1,10 +1,8 @@
 include(${CMAKE_CURRENT_LIST_DIR}/osqp.cmake)
 
 AddProject(osqp-eigen
-  # FIXME Revert this change when https://github.com/robotology/osqp-eigen/pull/131 (or equivalent) gets merged
-  # GITHUB robotology/osqp-eigen
-  # GIT_TAG origin/master
-  GITHUB gergondet/osqp-eigen
-  GIT_TAG origin/update-osqp-v1
+  GITHUB robotology/osqp-eigen
+  GIT_TAG origin/master
+  CMAKE_ARGS -DOSQP_IS_V1:BOOL=ON
   DEPENDS osqp
 )
