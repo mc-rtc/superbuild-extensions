529955a874ad12767a96283545978a73df90d847
Pierre Gergondet <pierre.gergondet@gmail.com>
2023-11-09 10:19:34 +0900

    [MuJoCo] Update to MuJoCo 3.0.0

--- simulation/MuJoCo.cmake
+++ simulation/MuJoCo.cmake
@@ -1,4 +1,4 @@
-set(MUJOCO_VERSION 2.3.7)
+set(MUJOCO_VERSION 3.0.0)
 
 if(UNIX AND NOT APPLE AND NOT EMSCRIPTEN)
   set(MUJOCO_URL "https://github.com/deepmind/mujoco/releases/download/${MUJOCO_VERSION}/mujoco-${MUJOCO_VERSION}-linux")
