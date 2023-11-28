c19586bda672de5e7051a1407108db9313e72ac1
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-11-28 10:08:21 +0100

    [xsens] Remove erroneous option and update download link

--- plugins/mc_xsens_plugin.cmake
+++ plugins/mc_xsens_plugin.cmake
@@ -1,8 +1,3 @@
-option(WITH_XSENS_PLUGIN "Build Xsens streaming plugin" OFF)
-if(NOT WITH_XSENS_PLUGIN)
-  return()
-endif()
-
 option(WITH_XSENS_STREAMING "Build Xsens streaming plugin with xsens_streaming/mtsdk (live mode)" ON)
 if(WITH_XSENS_STREAMING)
   include(${CMAKE_CURRENT_LIST_DIR}/mtsdk.cmake)
