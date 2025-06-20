094c9939e0bd589155c080d3ec364cd82076d22d
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-06-20 14:58:03 +0200

    feat: Install MuJoCo in CMAKE_INSTALL_PREFIX
    Ensure that MuJoCo is only installed once.
    This is needed by standalone docker images to keep all installed files in the same install prefix.

--- simulation/MuJoCo.cmake
+++ simulation/MuJoCo.cmake
@@ -1,6 +1,13 @@
 set(MUJOCO_VERSION 3.0.0)
 
+
 if(UNIX AND NOT APPLE AND NOT EMSCRIPTEN)
+  set(MUJOCO_ROOT_DIR "${CMAKE_INSTALL_PREFIX}/share/mujoco/mujoco-${MUJOCO_VERSION}")
+  if(MUJOCO_ROOT_DIR AND EXISTS ${MUJOCO_ROOT_DIR})
+    message(STATUS "MuJoCo is already installed in ${MUJOCO_ROOT_DIR}")
+    return()
+  endif()
+
   set(MUJOCO_URL "https://github.com/deepmind/mujoco/releases/download/${MUJOCO_VERSION}/mujoco-${MUJOCO_VERSION}-linux")
   if(${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
     set(MUJOCO_URL "${MUJOCO_URL}-aarch64.tar.gz")
@@ -12,7 +19,12 @@ if(UNIX AND NOT APPLE AND NOT EMSCRIPTEN)
         INPUT "${CMAKE_CURRENT_BINARY_DIR}/mujoco/mujoco.tar.gz"
         DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/mujoco"
   )
-  set(MUJOCO_ROOT_DIR "${CMAKE_CURRENT_BINARY_DIR}/mujoco/mujoco-${MUJOCO_VERSION}")
+  add_custom_target(install_mujoco ALL
+    COMMAND ${SUDO_OPT_CMD} ${CMAKE_COMMAND} -E copy_directory
+            "${CMAKE_CURRENT_BINARY_DIR}/mujoco"
+            "${CMAKE_INSTALL_PREFIX}/share/mujoco"
+    COMMENT "Copying MuJoCo to ${MUJOCO_ROOT_DIR}"
+  )
 else()
   if(NOT DEFINED MUJOCO_ROOT_DIR)
     message(FATAL_ERROR "MuJoCo must be installed manually on your platform, you must then defined MUJOCO_ROOT_DIR")
