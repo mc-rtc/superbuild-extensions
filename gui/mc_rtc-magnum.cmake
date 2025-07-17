972fc3cadaf6a583ebeb629923426ce051108f8a
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-07-17 17:59:33 +0200

    feat: update mc_rtc-magnum

--- gui/mc_rtc-magnum.cmake
+++ gui/mc_rtc-magnum.cmake
@@ -1,7 +1,7 @@
 AptInstall(libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libassimp-dev)
 
 AddProject(mc_rtc-magnum
-  GITHUB arntanguy/mc_rtc-magnum
+  GITHUB mc-rtc/mc_rtc-magnum
   GIT_TAG origin/main
   DEPENDS mc_rtc
 )
