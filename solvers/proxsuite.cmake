386a33132543a828f07f4bccaa40f12d86efa39b
ThomasDuvinage <toto.duvinage@gmail.com>
2026-02-27 11:38:43 +0900

    fix(proxsuite): update branch from main to devel

--- solvers/proxsuite.cmake
+++ solvers/proxsuite.cmake
@@ -2,6 +2,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/apt-dependencies.cmake)
 
 AddProject(proxsuite
   GITHUB Simple-Robotics/proxsuite
-  GIT_TAG origin/main
+  GIT_TAG origin/devel
   CMAKE_ARGS -DBUILD_TESTING=OFF -DBUILD_WITH_VECTORIZATION_SUPPORT=OFF
 )
